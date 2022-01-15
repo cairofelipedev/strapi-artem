359146929652e0be98569f99ebb05e0430395b30
cairofelipedev <cairofelipedev@gmail.com>
2022-01-15 14:06:47 -0300

    first deploy heroku

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'us-cdbr-east-05.cleardb.net'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi-artem'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
+        database: env('DATABASE_NAME', 'heroku_ae8279a2551dbfd'),
+        username: env('DATABASE_USERNAME', 'b487d79fefdbf7'),
+        password: env('DATABASE_PASSWORD', '9e76ebae'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
